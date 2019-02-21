d282253afcd21531aa4d1b9b1d3465e6d6009b71
corenel <xxdsox@gmail.com>
2019-02-21 15:45:50 +0800

    Add 125_valid_palindrome

--- algorithms/151_reverse_words_in_a_string.py
+++ algorithms/151_reverse_words_in_a_string.py
@@ -34,7 +34,8 @@ def reverse_words_in_a_string(string):
     :return: reversed string
     :rtype: str
     """
-    splited = [word for word in string.split(' ')
+    # return ' '.join(string.strip().split(' ')[::-1])
+    splited = [word for word in string.strip().split(' ')
                if word != '']
     return ' '.join(splited[::-1])
 
